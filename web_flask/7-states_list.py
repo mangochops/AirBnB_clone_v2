ede386a42dce23f84e4eb217a36dd203d4b3d698
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-20 13:31:48 +0300

    Create 7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Starts a Flask web application to display states list"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays a HTML page with the list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the current SQLAlchemy session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
