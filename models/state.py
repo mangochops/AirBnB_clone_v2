8f0de9e9f2b18ac5522eb82662f3ac8becdc9230
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-20 13:26:47 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -6,3 +6,13 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+    @property
+    def cities(self):
+        """Returns the list of City objects from storage linked to the current State"""
+        if getenv('HBNB_TYPE_STORAGE') != 'db':
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+        return self.cities  # SQLAlchemy relationship attribute
