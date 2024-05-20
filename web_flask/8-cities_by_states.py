8a9ba639d2cd4ad5500a16a82deeed5da1d5d6b3
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-20 13:50:23 +0300

    Create 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for starting a Flask web application."""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+@app.route('/cities_by_states', methods=['GET'])
+def cities_by_states():
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
