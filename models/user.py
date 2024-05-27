252608c91494e8e11560b33617768726f817d14e
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-27 20:42:26 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,30 @@
-#!/usr/bin/python3
+import hashlib
+from models.base_model import BaseModel
+ 
 """This module defines a class User"""
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    def __init__(self, *args, **kwargs):
+        """Initialize User object."""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """Getter method for password."""
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        """Setter method for password, hashes the password to MD5."""
+        md5_hash = hashlib.md5(value.encode()).hexdigest()
+        self.__password = md5_hash
+   
+    
+    
+    class User(BaseModel):
+        """This class defines a user by various attributes"""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
