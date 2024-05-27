acb3de74e3ee0c5e83f3360f8fa358b213066d43
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-27 20:01:24 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Initialize the API views module"""
+"""
+Initialize the views module
+"""
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.cities import *  # Add this line
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
 
