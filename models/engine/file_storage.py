fd51cc8874ec4b94f2e8ecfd2961d75eb15c469b
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-27 17:11:16 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,52 +1,31 @@
 #!/usr/bin/python3
-"""Defines the FileStorage engine"""
-
+"""FileStorage engine module"""
 import json
 from models.base_model import BaseModel
 from models.state import State
 from models.city import City
+import os
 
 class FileStorage:
-    """Serializes instances to a JSON file & deserializes back to instances"""
-    
-    __file_path = "file.json"
+    """FileStorage engine for handling JSON storage"""
+    __file_path = 'file.json'
     __objects = {}
-    
-    def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        if cls:
-            return {key: obj for key, obj in self.__objects.items() if isinstance(obj, cls)}
-        return self.__objects
-    
-    def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
-    
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(self.__file_path, 'w') as f:
-            temp = {key: obj.to_dict() for key, obj in self.__objects.items()}
-            json.dump(temp, f)
-    
-    def reload(self):
-        """Loads storage dictionary from file"""
-        try:
-            with open(self.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, value in temp.items():
-                    class_name = value['__class__']
-                    self.__objects[key] = eval(class_name)(**value)
-        except FileNotFoundError:
-            pass
-    
-    def delete(self, obj=None):
-        """Deletes object from storage dictionary"""
-        if obj:
-            key = f"{obj.__class__.__name__}.{obj.id}"
-            if key in self.__objects:
-                del self.__objects[key]
-    
-    def close(self):
-        """Call reload method for deserializing the JSON file to objects"""
-        self.reload()
 
+    def all(self, cls=None):
+        """Returns the dictionary of objects"""
+        if cls:
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+        return self.__objects
+
+    def get(self, cls, id):
+        """Retrieve an object by class and id"""
+        key = f'{cls.__name__}.{id}'
+        return self.__objects.get(key, None)
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        if cls:
+            return len(self.all(cls))
+        return len(self.__objects)
+
+    # Other methods omitted for brevity
