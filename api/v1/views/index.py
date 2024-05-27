3a5974e6ef2261a3214b28b2b86ec8c6bc939777
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-27 17:51:25 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-"""Index view module"""
+"""Module for handling API routes"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves the number of each objects by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
