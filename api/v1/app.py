d1207e2676204785e12121612d93158de881befa
mangochops <133221712+mangochops@users.noreply.github.com>
2024-05-27 18:01:26 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""API setup and main application instance"""
-from flask import Flask
+"""Module for setting up the Flask application"""
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -10,10 +10,15 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the storage on teardown"""
+    """Closes the storage on teardown"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors and returns a JSON response"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+    port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
