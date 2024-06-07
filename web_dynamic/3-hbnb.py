6673419143730b880d484839d678651b8dcb3d26
mangochops <133221712+mangochops@users.noreply.github.com>
2024-06-07 20:46:13 +0300

    Create 3-hbnb.py

---
+++ web_dynamic/3-hbnb.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import uuid
+
+app = Flask(__name__)
+
+@app.route('/3-hbnb/')
+def display_hbnb():
+    cache_id = uuid.uuid4()
+    return render_template('3-hbnb.html', cache_id=cache_id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
